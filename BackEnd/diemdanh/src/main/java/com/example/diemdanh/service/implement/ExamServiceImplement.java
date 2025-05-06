eafe89c872ca8d2c4c151c7df0b98b468cbdc6df
Tucccccc <94109738+Tucccccc@users.noreply.github.com>
2025-05-06 21:58:14 +0700

    Update ExamServiceImplement.java

--- BackEnd/diemdanh/src/main/java/com/example/diemdanh/service/implement/ExamServiceImplement.java
+++ BackEnd/diemdanh/src/main/java/com/example/diemdanh/service/implement/ExamServiceImplement.java
@@ -1,12 +1,16 @@
 package com.example.diemdanh.service.implement;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.diemdanh.dto.ExamDTO;
+import com.example.diemdanh.repository.ExamRepository;
 import com.example.diemdanh.service.ExamService;
 
 @Service
-public class ExamServiceImplement implements ExamService{
+public class ExamServiceImplement implements ExamService {
+	@Autowired
+	private ExamRepository examRepository;
 
 	@Override
 	public ExamDTO addExam(ExamDTO examRequest) {
